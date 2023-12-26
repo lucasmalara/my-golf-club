000ec95943419c222c238fc367557c5aa09b9812
Lucas <malara.career.d9mwd@slmail.me>
2023-12-26 12:17:33 +0100

    Updated: Spring Boot to version 3.2.1; dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.mygolfclub"
@@ -21,19 +21,25 @@ repositories {
 	mavenCentral()
 }
 
+val versionSpringBootStarter: String = "3.2.1"
+val versionThymeleafSecExtras: String = "3.1.2.RELEASE"
+val versionOpenAPI: String = "2.3.0"
+val versionLombok: String = "1.18.30"
+val versionMySQLCon: String = "8.2.0"
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.5")
-	implementation("org.springframework.boot:spring-boot-starter-security:3.1.5")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.1.5")
-	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:3.1.2.RELEASE")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
-	implementation("org.springframework.boot:spring-boot-starter-hateoas:3.1.5")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-	compileOnly("org.projectlombok:lombok:1.18.30")
-	runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
-	annotationProcessor("org.projectlombok:lombok:1.18.30")
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:$versionSpringBootStarter")
+	implementation("org.springframework.boot:spring-boot-starter-security:$versionSpringBootStarter")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:$versionSpringBootStarter")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:$versionThymeleafSecExtras")
+	implementation("org.springframework.boot:spring-boot-starter-validation:$versionSpringBootStarter")
+	implementation("org.springframework.boot:spring-boot-starter-web:$versionSpringBootStarter")
+	implementation("org.springframework.boot:spring-boot-starter-hateoas:$versionSpringBootStarter")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$versionOpenAPI")
+	compileOnly("org.projectlombok:lombok:$versionLombok")
+	runtimeOnly("com.mysql:mysql-connector-j:$versionMySQLCon")
+	annotationProcessor("org.projectlombok:lombok:$versionLombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:$versionSpringBootStarter")
 }
 
 tasks.withType<Test> {
