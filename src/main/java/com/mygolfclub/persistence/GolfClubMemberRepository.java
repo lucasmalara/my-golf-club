5bbb9878568af2b60da0b1352290e30a10e429a7
Lucas <malara.career.d9mwd@slmail.me>
2023-12-28 20:14:57 +0100

    Added a new custom query method for GolfClubMemberRepository.

--- src/main/java/com/mygolfclub/persistence/GolfClubMemberRepository.java
+++ src/main/java/com/mygolfclub/persistence/GolfClubMemberRepository.java
@@ -2,9 +2,16 @@ package com.mygolfclub.persistence;
 
 import com.mygolfclub.entity.member.GolfClubMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GolfClubMemberRepository extends JpaRepository<GolfClubMember, Integer> {
+
     List<GolfClubMember> findAllByActiveMember(boolean isActiveMember);
+
+    @Query("select m from GolfClubMember m where m.firstName=:firstName and m.lastName=:lastName")
+    List<GolfClubMember> findByFirstAndLastName(@Param("firstName") String firstName,
+                                                @Param("lastName") String lastName);
 }
