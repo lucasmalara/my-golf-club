81c210513f1e5b3620a2e835c21c4319608b40b4
Lucas <malara.career.d9mwd@slmail.me>
2024-01-17 12:58:20 +0100

    Added annotation elements with default values.

--- src/test/java/com/mygolfclub/utils/InjectMember.java
+++ src/test/java/com/mygolfclub/utils/InjectMember.java
@@ -8,4 +8,11 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface InjectMember {
+    String firstName() default "Lorem";
+
+    String lastName() default "Ipsum";
+
+    String email() default "dolor@sit.amet";
+
+    boolean active() default true;
 }
